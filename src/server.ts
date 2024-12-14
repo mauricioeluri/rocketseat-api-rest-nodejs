076515c5e73d59dac99438a11d5cd370efd6298d
Maurício El Uri <mauriciom.eluri@gmail.com>
2024-12-14 07:46:32 -0300

    routes and transaction plugin

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions').select('*')
-
-  return transaction
-})
+app.register(transactionsRoutes, { prefix: '/transactions' })
 
 app
   .listen({
     port: env.PORT,
   })
   .then(() => {
-    console.log('Server is running on port 80')
+    console.log(`Server is running on port ${env.PORT}`)
   })
